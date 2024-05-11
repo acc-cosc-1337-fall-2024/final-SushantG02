36b1f41168166e37a1274808ad02022e3e571eb6
SushantG02 <157077888+SushantG02@users.noreply.github.com>
2024-05-11 15:28:27 +0000

    Final exam problem 1.

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,10 @@
 //
+#include "die.h"
+
+Die::Die() : sides(6), generator(std::random_device{}()) {}
+
+int Die::roll() 
+{
+    std::uniform_int_distribution<int> distribution(1, sides);
+    return distribution(generator);
+}
--- src/die.h
+++ src/die.h
@@ -1,1 +1,17 @@
-//
+#ifndef DIE_H
+#define DIE_H
+
+#include <random>
+
+class Die 
+{
+public:
+    Die();
+    int roll();
+
+private:
+    int sides;
+    std::default_random_engine generator;
+};
+
+#endif
